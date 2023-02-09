35fe98722240ab32bdfc9eb826732bd94bdaf4b9
Swarangi <swarangi.kulkarni@tcs.com>
2023-02-09 18:44:22 +0000

    Day18

---
+++ Day18/spring-rest-communication/spring-caller/src/test/java/com/swarangi/rest/CallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swarangi.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
